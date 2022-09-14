498c5b342458d43e52e2107f788d03c0951894f8
JPonda <pondajay637@gmail.com>
2022-09-15 00:37:20 +0530

    practical 2: gridView [1]

--- app/src/main/java/com/demo/labpracticals/GridViewDemoActivity.java
+++ app/src/main/java/com/demo/labpracticals/GridViewDemoActivity.java
@@ -2,20 +2,15 @@ package com.demo.labpracticals;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
 
-import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import com.demo.labpracticals.adapter.GridViewCustomAdapter;
 import com.demo.labpracticals.data.Animals;
 import com.demo.labpracticals.data.StaticData;
 import com.demo.labpracticals.databinding.ActivityGridViewDemoBinding;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 public class GridViewDemoActivity extends AppCompatActivity {
@@ -30,14 +25,9 @@ public class GridViewDemoActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         int orientation = getResources().getConfiguration().orientation;
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(
-                this,
-                orientation == Configuration.ORIENTATION_PORTRAIT ? 3 : 4,
-                GridLayoutManager.VERTICAL,
-                false);
-        binding.recyclerViewDemo.setLayoutManager(gridLayoutManager);
+        binding.gridBaseComp.setNumColumns(orientation == Configuration.ORIENTATION_PORTRAIT ? 3 : 4);
         Animals[] animals = StaticData.getInstance().getAnimalList(5);
-        binding.recyclerViewDemo.setAdapter(new GridViewCustomAdapter(animals));
+        binding.gridBaseComp.setAdapter(new GridViewCustomAdapter(animals));
 
     }
 
--- app/src/main/java/com/demo/labpracticals/adapter/GridViewCustomAdapter.java
+++ app/src/main/java/com/demo/labpracticals/adapter/GridViewCustomAdapter.java
@@ -3,11 +3,9 @@ package com.demo.labpracticals.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.demo.labpracticals.R;
 import com.demo.labpracticals.data.Animals;
@@ -15,7 +13,7 @@ import com.demo.labpracticals.data.StaticData;
 
 import java.util.Arrays;
 
-public class GridViewCustomAdapter extends RecyclerView.Adapter<GridViewCustomAdapter.GridHolder> {
+public class GridViewCustomAdapter extends BaseAdapter {
 
     private final Animals[] animalList;
 
@@ -29,45 +27,34 @@ public class GridViewCustomAdapter extends RecyclerView.Adapter<GridViewCustomAd
     }
 
 
-
-    @NonNull
     @Override
-    public GridHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.grid_layout_templete, parent, false);
-
-        return new GridHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull GridHolder holder, int position) {
-        holder.getImageView().setImageResource(animalList[position].getImgRes());
-        holder.getTextView().setText(animalList[position].getName());
-    }
-
-    @Override
-    public int getItemCount() {
+    public int getCount() {
         return animalList.length;
     }
 
-    public static class GridHolder extends RecyclerView.ViewHolder {
+    @Override
+    public Object getItem(int position) {
+        return animalList[position];
+    }
 
-        private final ImageView imageView;
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-        public TextView getTextView() {
-            return textView;
-        }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        Animals animals = animalList[position];
+        if(view == null)
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_layout_templete, parent, false);
 
-        private final TextView textView;
+        ImageView imageView = view.findViewById(R.id.demoImage);
+        TextView textView = view.findViewById(R.id.demoText);
 
-        public GridHolder(View view){
-            super(view);
-            imageView = view.findViewById(R.id.demoImage);
-            textView = view.findViewById(R.id.demoText);
-        }
+        imageView.setImageResource(animals.getImgRes());
+        textView.setText(animals.getName());
 
-        public ImageView getImageView() {
-            return imageView;
-        }
+        return view;
     }
 }
