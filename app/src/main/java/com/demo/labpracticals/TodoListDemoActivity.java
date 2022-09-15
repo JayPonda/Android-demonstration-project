0147c926d5a8052d55fb62ad07c740364a77cf5d
JPonda <pondajay637@gmail.com>
2022-09-15 17:03:49 +0530

    practical 6: todoList

--- app/src/main/java/com/demo/labpracticals/TodoListDemoActivity.java
+++ app/src/main/java/com/demo/labpracticals/TodoListDemoActivity.java
@@ -1,35 +1,90 @@
 package com.demo.labpracticals;
 
-import static com.demo.labpracticals.MainActivity.CLASS_NAME;
-import static com.demo.labpracticals.MainActivity.DATA_VALUE;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
+import com.demo.labpracticals.databinding.ActivityTodoListDemoBinding;
+
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class TodoListDemoActivity extends AppCompatActivity {
 
+    ActivityTodoListDemoBinding binding;
+    ArrayList<String> todos = new ArrayList<>();
+    Toast toast;
+    Boolean isUpdate = false;
+    int itemPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todo_list_demo);
-
-        Intent intent = getIntent();
-        int it = intent.getIntExtra(DATA_VALUE, -1);
-        String str = intent.getStringExtra(CLASS_NAME);
-        TextView text = findViewById(R.id.textView);
-        str = str + " " + it;
-        text.setText(str);
+        binding = ActivityTodoListDemoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        // Read
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todos);
+        binding.listViewTodos.setAdapter(adapter);
+
+        // Write
+        binding.submitFormElm.setOnClickListener(v -> {
+            String newTodo = binding.todosFormElm.getText().toString();
+            if(newTodo.isEmpty())
+                showToast("empty todo can not be inserted");
+            else{
+                binding.todosFormElm.setText("");
+                if(isUpdate){
+                    todos.set(itemPosition, newTodo);
+                    isUpdate = false;
+                    binding.submitFormElm.setText(getString(R.string.add));
+                }
+                else
+                    todos.add(newTodo);
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        // Update
+        binding.listViewTodos.setOnItemClickListener((parent, view, position, id) -> {
+            String oldVal = todos.get(position);
+            binding.todosFormElm.setText(oldVal);
+            showToast("you can update value from above control");
+            binding.submitFormElm.setText(getString(R.string.edit));
+            isUpdate = true;
+            itemPosition = position;
+        });
+
+        // Delete
+        binding.listViewTodos.setOnItemLongClickListener((parent, view, position, id) -> {
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.setTitle("Delete Alert")
+                    .setMessage("are you sure, you want to delete this item?")
+                    .setPositiveButton(getString(R.string.yes_delete), (dialog, which) -> {
+                        todos.remove(position);
+                        adapter.notifyDataSetChanged();
+                    }) .setNegativeButton(getString(R.string.cancel), (dialog, which) -> dialog.dismiss()).show();
+            alertBuilder.create();
+            return true;
+        });
     }
 
+    // helper
+    private void showToast(String msg){
+        if(toast != null)
+            toast.cancel();
+        toast = Toast.makeText(TodoListDemoActivity.this, msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    // set Back button
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == android.R.id.home){
@@ -39,4 +94,5 @@ public class TodoListDemoActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
